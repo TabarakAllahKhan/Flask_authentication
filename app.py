17664e41c798b1fcba599c4daeab7a13687b38db
Tabarak Allah <tabarakalizai861@gmail.com>
2025-09-01 09:24:36 +0500

    `Added import of request and debug run configuration`

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect,request
 
 
 app=Flask(__name__)
 
+
+
+
+
+if __name__=='main':
+    app.run(debug=True)
